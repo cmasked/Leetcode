badd3de976fdeda081c96853ff0eea211f0b0edd
Chirag Mehta <chiragmehta6oct@gmail.com>
2026-03-06 17:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (89.70%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0=0;
+        int count1=0;
+        int count2=0; 
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }}
+        for(int j=0;j<nums.length;j++){
+            if(count0>0){
+                nums[j]=0;
+                count0--;
+            }
+                else if(count1>0){
+                     nums[j]=1;
+                    count1--;
+            }
+                else if(count2>0){
+                    
+                nums[j]=2;
+                count2--;
+            }
+                
+        } 
+            
+        
+        
+    }
+}

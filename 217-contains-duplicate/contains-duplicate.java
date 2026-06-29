6faaf7aa34316363a7187d78c57fdd3bfea9c062
Chirag Mehta <chiragmehta6oct@gmail.com>
2026-06-29 15:30:52 +0530

    Time: 20 ms (32.06%) | Memory: 108.1 MB (6.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,12 +3,11 @@ class Solution {
 
         HashSet<Integer> map= new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            if(!map.contains(nums[i])){
-                map.add(nums[i]);
-            }
-            else{
+            if(map.contains(nums[i])){
                 return true;
             }
+            map.add(nums[i]);
+            
         }
         return false;
     }

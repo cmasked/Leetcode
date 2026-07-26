adc79d7a28db0f97b6b4e3c3159061a3ca96e076
Chirag Mehta <chiragmehta6oct@gmail.com>
2026-07-26 15:31:02 +0530

    Time: 7 ms (35.24%) | Memory: 45.2 MB (79.27%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,9 +3,6 @@ class Solution {
         Stack<Integer> stack=new Stack<>();
         int total=0;
         for(int i=0;i<tokens.length;i++){
-            if(tokens.length==1){
-                total=Integer.parseInt(tokens[i]);
-            }
             if(!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("/") && !tokens[i].equals("*")   ){
                 stack.push(Integer.parseInt(tokens[i]));
                             }
@@ -17,22 +14,22 @@ class Solution {
                     total=a+b;
                     stack.push(total);
                 }
-                if(tokens[i].equals("-")){
+                else if(tokens[i].equals("-")){
                     total=a-b;
                     stack.push(total);
                 }
-                if(tokens[i].equals("/")){
+                else if(tokens[i].equals("/")){
                     total=a/b;
                     stack.push(total);
                 }
-                if(tokens[i].equals("*")){
+                else{
                     total=a*b;
                     stack.push(total);
                 }
             }
 
         }
-        return total;
+            return stack.pop();
         
     }
 }

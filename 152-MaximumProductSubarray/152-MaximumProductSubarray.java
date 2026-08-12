63eb8059188777012c7fb4e16d6c196a635d9a43
Chirag Mehta <chiragmehta6oct@gmail.com>
2026-08-12 23:59:21 +0530

    File updated at 12/08/2026, 23:59:20

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,30 +1,27 @@
-// Last updated: 12/08/2026, 23:34:39
+// Last updated: 12/08/2026, 23:59:20
 1class Solution {
 2    public int maxProduct(int[] nums) {
 3        
 4        
 5        int oldmax=nums[0];
 6        int oldmin=nums[0];
-7        int newmax=nums[0];
-8        int newmin=nums[0];
-9        int answer=nums[0];
-10        int max=nums[0];
-11        int min=nums[0];
-12
-13        for(int i=1;i<nums.length;i++){
-14            
-15            max=Math.max(nums[i],Math.max(oldmax*nums[i],oldmin*nums[i]));
-16            min=Math.min(nums[i],Math.min(oldmax*nums[i],newmin*nums[i]));
-17            newmax=max;
-18            newmin=min;
-19            answer=Math.max(answer,newmax);
-20            oldmax=max;
-21            oldmin=min;
-22            
-23            
+7        
+8        int answer=nums[0];
+9        int max=nums[0];
+10        int min=nums[0];
+11
+12        for(int i=1;i<nums.length;i++){
+13            
+14            max=Math.max(nums[i],Math.max(oldmax*nums[i],oldmin*nums[i]));
+15            min=Math.min(nums[i],Math.min(oldmax*nums[i],oldmin*nums[i]));
+16            answer=Math.max(answer,max);
+17            oldmax=max;
+18            oldmin=min;
+19            
+20            
+21
+22        }
+23        return answer;
 24
-25        }
-26        return answer;
-27
-28    }}
-29      
+25    }}
+26      
